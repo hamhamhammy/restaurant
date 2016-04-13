2be615c09d2ee622e599e31301debb182a4f52f0
hamhamhammy <hamzeh.doostie@gmail.com>
2016-04-13 17:54:47 -0400

    API - Login

--- restaurant/api/auth/urls.py
+++ restaurant/api/auth/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns(
 
     # This is just to access the register view with a reverse
     url(r'^register$', views.register, name='api-auth-register'),
+    url(r'^login$', views.login, name='api-auth-login'),
 )
--- restaurant/api/auth/views.py
+++ restaurant/api/auth/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views.decorators.csrf import csrf_exempt
+
+from django.contrib import auth
 from django.contrib.auth.models import User
 
 @csrf_exempt
@@ -23,3 +25,20 @@ def register(request):
 
     else:
         return HttpResponse('GET api/auth/register')
+
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            # Correct password, and the user is marked "active"
+            auth.login(request, user)
+            # Redirect to a success page.
+            return HttpResponse("login success")
+        else:
+            # Show an error page
+            return HttpResponse("login failed")
+    else:
+        return HttpResponse('GET api/auth/login')
