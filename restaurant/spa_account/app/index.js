11d6c36db2b391c89f08df700de962e4cf6829fa
hamhamhammy <hamzeh.doostie@gmail.com>
2016-04-19 15:05:03 -0400

    Live reload and css

--- restaurant/spa_account/app/index.js
+++ restaurant/spa_account/app/index.js
@@ -1,1 +1,33 @@
-console.log('hello from spa_account webpack');
+// ES6 polyfills for Internet Explorer
+import 'babel-polyfill';
+// This is required to get babel compiled
+// code to work on IE10
+// They should fix it soon but until then
+// we need this
+// more info https://phabricator.babeljs.io/T3041
+import 'ima.js-babel6-polyfill';
+
+// Dependencies
+import angular from 'angular';
+import ngResource from 'angular-resource';
+import uiBootstrap from 'angular-ui-bootstrap';
+import uiRouter from 'angular-ui-router';
+
+// Modules
+//import LoginModule from './modules/login';
+
+// Global style
+import './main.scss';
+
+export default angular
+    .module('spa-account', [
+        // Dependencies
+        ngResource,
+        uiBootstrap,
+        uiRouter,
+    ]);
+    // .config(routes)
+    // .config(RouteConfig)
+    // .run(Replay);
+
+console.log('hello from spa_account webpack livereload');
--- restaurant/spa_account/views.py
+++ restaurant/spa_account/views.py
@@ -1,3 +1,6 @@
+import os
+import socket
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
@@ -7,9 +10,17 @@ from django.core.urlresolvers import reverse
 from restaurant.api.auth.views import logout as api_logout
 
 # Create your views here.
+def get_live_reload_url():
+    """Change the port in a URL."""
+    print "hamzeh socket.getfqdn() = " + str(socket.getfqdn())
+    livereload_port = os.environ.get('LIVE_RELOAD_PORT', 35279)
+    return '//%s:%s/livereload.js' % ('159.203.251.105', livereload_port) #socket.getfqdn()
+
 def main(request):
     template_name='spa_account/index.html'
-    return render(request, template_name)
+    return render(request, template_name, {
+        'live_reload_url': get_live_reload_url()
+    })
 
 def login(request):
     if request.user.is_authenticated():
